86c09077527238ad130e9353e0fe95668ec0d11d
MODMYPI <dave@modmypi.com>
2016-08-31 16:50:27 +0100

    Rotary Script

---
+++ rotary_encoder.py
@@ -0,0 +1,27 @@
+from RPi import GPIO
+from time import sleep
+
+a = 17
+b = 18
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(a, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(b, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+counter = 0
+aLastState = GPIO.input(a)
+
+try:
+
+        while True:
+                aState = GPIO.input(a)
+                if aState != aLastState:
+                        if GPIO.input(b) != aState:
+                                counter -= 1
+                        else:
+                                counter += 1
+                        print counter
+                aLastState = aState
+                sleep(0.01)
+finally:
+        GPIO.cleanup()
