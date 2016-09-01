5211eb336ef1e533c631989659654b91966ccab2
MODMYPI <dave@modmypi.com>
2016-09-01 13:28:00 +0100

    Actually updated variable names

--- rotary_encoder.py
+++ rotary_encoder.py
@@ -9,19 +9,20 @@ GPIO.setup(clk, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(dt, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 counter = 0
-aLastState = GPIO.input(clk)
+clkLastState = GPIO.input(clk)
 
 try:
 
         while True:
-                aState = GPIO.input(clk)
-                if aState != aLastState:
-                        if GPIO.input(b) != aState:
+                clkState = GPIO.input(clk)
+				dtState = GPIO.input(dt)
+                if clkState != clkLastState:
+                        if dtState != aState:
                                 counter += 1
                         else:
                                 counter -= 1
                         print counter
-                aLastState = aState
+                clkLastState = clkState
                 sleep(0.01)
 finally:
         GPIO.cleanup()
